a7db79f01d43a04b71917d6b74a6866755a1bd35
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-01-30 18:01:04 +0800

    Initial Commit

---
+++ SpectaExpectaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpectaExpectaDemo
+//
+//  Created by Ricardo Pramana Suranta on 30/01/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpectaExpectaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpectaExpectaDemo
+//
+//  Created by Ricardo Pramana Suranta on 30/01/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
